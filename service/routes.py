9d2fbf7e4f3ee861f8499f51b3b6a3a054bdd9b9
Viktor151274 <viktor151274@gmail.com>
2025-06-01 16:05:05 +0300

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -62,7 +62,6 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 
-
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -82,7 +81,6 @@ def list_accounts():
 # READ AN ACCOUNT
 ######################################################################
 
-
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     """
@@ -99,6 +97,7 @@ def get_accounts(account_id):
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
+
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
@@ -118,7 +117,6 @@ def update_accounts(account_id):
 # DELETE AN ACCOUNT
 ######################################################################
 
-
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
     """
@@ -137,6 +135,7 @@ def delete_accounts(account_id):
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
+
 
 def check_content_type(media_type):
     """Checks that the media type is correct"""
